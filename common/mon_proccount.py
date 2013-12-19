f6aed201ec9f32c7828a20f6b0e495c1323f4c89
pztrn <pztrn@pztrn.ru>
2013-12-20 02:46:17 +0400

    mon_proccount: allow to count by parameter name.

--- common/mon_proccount.py
+++ common/mon_proccount.py
@@ -7,6 +7,7 @@
 
 import sys
 import argparse
+import subprocess
 
 try:
     import psutil
@@ -31,18 +32,32 @@ class Process_Monitor:
         # Process name to count.
         # Counting process instances.
         count = 0
-        for process in psutil.process_iter():
-            if process.name == self.args.PROCESS:
-                count += 1
+        if self.args.PROCESS:
+            opt = self.args.PROCESS
+            for process in psutil.process_iter():
+                if process.name == self.args.PROCESS:
+                    count += 1
+        
+        if self.args.PARM:
+            opt = self.args.PARM
+            p = subprocess.Popen(["ps", "ax"], stdout = subprocess.PIPE)
+            data = p.communicate()[0]
+            data = str(data).split(r"\n")
+            for item in data:
+                if self.args.PARM in item:
+                    count += 1
+                    
+            # One of detected values - ps itself.
+            count -= 1
                 
         if count <= CRIT_VALUE:
-            print(self.args.PROCESS.upper() + " CRITICAL: {0} instances running".format(count))
+            print(opt.upper() + " CRITICAL: {0} instances running".format(count))
             exit(2)
         elif count > CRIT_VALUE and count <= WARN_VALUE:
-            print(self.args.PROCESS.upper() + " WARNING: {0} instances running".format(count))
+            print(opt.upper() + " WARNING: {0} instances running".format(count))
             exit(1)
         else:
-            print(self.args.PROCESS.upper() + " OK: {0} instances running".format(count))
+            print(opt.upper() + " OK: {0} instances running".format(count))
             exit(0)
         
     def parse_args(self):
@@ -51,6 +66,7 @@ class Process_Monitor:
         """
         opts = argparse.ArgumentParser(description='Process count monitor', epilog="Monitor processes by count.")
         opts.add_argument("-p", help="Process name", metavar="PROCESSNAME", action="store", dest="PROCESS")
+        opts.add_argument("-o", help="Process parameter", metavar="PROCESSNAME", action="store", dest="PARM")
         opts.add_argument("-w", help="Warning value", metavar="WARN_VALUE", action="store", dest="WARNING")
         opts.add_argument("-c", help="Critical value", metavar="CRIT_VALUE", action="store", dest="CRITICAL")
         self.args = opts.parse_args()
