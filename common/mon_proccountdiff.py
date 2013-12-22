dff07212f1ac294b4bb41bf15737c2d6af6509af
pztrn <pztrn@pztrn.ru>
2013-12-22 13:05:27 +0400

    moc_proccountdiff: one more comparsion fix

--- common/mon_proccountdiff.py
+++ common/mon_proccountdiff.py
@@ -81,7 +81,7 @@ class Process_Monitor:
         # Getting difference.
         diff = count_proc1 - count_proc2
         
-        if do_criticals and diff <= CRIT_VALUE:
+        if do_criticals and diff >= CRIT_VALUE:
             print(alert_name + " CRITICAL: difference is {0} ({1} - {2}, {3} - {4})".format(diff, self.args.PROCESS_ONE, count_proc1, self.args.PROCESS_TWO, count_proc2))
             exit(2)
         elif do_warnings and diff < CRIT_VALUE and diff >= WARN_VALUE:
