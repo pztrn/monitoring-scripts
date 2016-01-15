7fd0f3b00121d0c451bc1409aad257bc4761cff8
Stanislav N. aka pztrn <pztrn@pztrn.name>
2016-01-15 15:56:45 +0500

    chack_domainexp: search for whois and exit, if not found. Also added ability to check com domains.

--- common/check_domainexp.sh
+++ common/check_domainexp.sh
@@ -6,7 +6,7 @@
 
 # Getting domain information.
 function get_info() {
-    domaininfo=$(whois $domain)
+    domaininfo=$(${whois} ${domain})
     # Getting zone information. This is neccessary, because some zones
     # may have different substring for domain expiratin value.
     zone=`echo $domain | cut -d "." -f 2`
@@ -16,9 +16,17 @@ function get_info() {
         expire=`echo $domaininfo | awk {' print $15 '} | cut -d "T" -f 1`
     elif [ $zone == "so" ]; then
         expire=`echo $domaininfo | awk {' print $11 '} | cut -d ":" -f 2 | cut -d "T" -f 1`
+    elif [ $zone == "com" ]; then
+        #echo $domaininfo
+        expire=`echo -e ${domaininfo} | grep "Registrar Registration Expiration" | awk {' print $448 '} | cut -d "T" -f 1`
+        echo $expire
+    else
+        echo "Unsupported zone: ${zone}"
+        exit 3
     fi
 }
 
+# Dates compare.
 function compare_dates() {
     # Try to replace dots with dashes, if present.
     expire=$(echo $expire | sed -e "s/\./\-/g")
@@ -27,7 +35,7 @@ function compare_dates() {
     expire=$[ $expire-$curdate ]
     expire=$[ $expire / 60 / 60 / 24 ]
     #echo "$domain expires within $expire days"
-    
+
     # Compare dates and produce exitcodes.
     if [ $expire -le $warn ]; then
         if [ $expire -le $crit ]; then
@@ -47,15 +55,26 @@ function help() {
     echo "Domain expiration checker for Nagios/Icinga.
 Checks domains in these zones:
 
-    org, ru, so
+    com, org, ru, so
 
 Usage:
     mon_domainexp.sh [domain] [warn] [crit]
-    
+
 Available options:
     [domain]        Domain to check.
     [warn]          Produce warning if domain will expire in this days.
     [crit]          Produce critical if domain will expire in this days."
+}
+
+# Executes some preparation actions, e.g. searching for whois program.
+function prepare() {
+    whois_raw=`whereis whois`
+    if [ ${#whois_raw} -lt 10 ]; then
+        echo "Whois program not found! Cannot continue!"
+        exit 3
+    else
+        whois=`echo ${whois_raw} | awk {' print $2 '}`
+    fi
 }
 
 case $1 in
@@ -66,6 +85,7 @@ case $1 in
         domain=$1
         warn=$2
         crit=$3
+        prepare
         get_info
         compare_dates
 esac
